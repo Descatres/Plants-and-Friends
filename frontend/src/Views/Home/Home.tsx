73d460137cecd9dc321d0a0e24605f1ed19a723c
João Catré <joaomcatre@hotmail.com>
2024-12-17 23:47:22 +0000

    backup
    Co-Authored-By: Tiago Oliveira <69111007+toliveir4@users.noreply.github.com>

--- frontend/src/Views/Home/Home.tsx
+++ frontend/src/Views/Home/Home.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import classes from "./Home.module.css";
 import PlantCard from "../../Components/PlantCard/PlantCard";
 import Spinner from "../../Components/Spinner/Spinner";
-import Error from "../../Components/Errors/Errors";
 import { useFetchPlants } from "../../hooks/useFetchPlants";
 import RoomStats from "../../Components/RoomStats/RoomStats";
 import ListGridSwapper from "../../Components/ListGridSwapper/ListGridSwapper";
 import Search from "../../Components/Search/Search";
 import DropdownMenu from "../../Components/DropdownMenu/DropdownMenu";
+import ErrorPage from "../../errorPages/ErrorPage";
+import { useNavigate } from "react-router-dom";
+import { createPortal } from "react-dom";
 
 function Home() {
   const { getAllPlants, isLoadingPlants, plants, errorFindingData } =
@@ -16,6 +18,7 @@ function Home() {
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [sortBy, setSortBy] = useState<string | "">("");
   const [isDescending, setIsDescending] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getAllPlants();
@@ -112,28 +115,26 @@ function Home() {
         </div>
         <div className={classes.plantsContainer}>
           <div className={isList ? classes.plantRowList : classes.plantRowGrid}>
-            {filteredPlants && filteredPlants.length > 0 ? (
-              filteredPlants.map((plant, index) => (
-                <div key={index}>
-                  <div key={plant.id} className={classes.plant}>
-                    <PlantCard
-                      id={plant.name}
-                      name={plant.name}
-                      species={plant.species ?? ""}
-                      minTemperature={plant.minTemperature ?? undefined}
-                      maxTemperature={plant.maxTemperature ?? undefined}
-                      minHumidity={plant.minHumidity ?? undefined}
-                      maxHumidity={plant.maxHumidity ?? undefined}
-                      imageUrl={plant.imageUrl ?? ""}
-                      isList={isList}
-                    />
+            {filteredPlants && filteredPlants.length > 0
+              ? filteredPlants.map((plant, index) => (
+                  <div key={index}>
+                    <div key={plant.id} className={classes.plant}>
+                      <PlantCard
+                        id={plant.name}
+                        name={plant.name}
+                        species={plant.species ?? ""}
+                        minTemperature={plant.minTemperature ?? undefined}
+                        maxTemperature={plant.maxTemperature ?? undefined}
+                        minHumidity={plant.minHumidity ?? undefined}
+                        maxHumidity={plant.maxHumidity ?? undefined}
+                        imageUrl={plant.imageUrl ?? ""}
+                        isList={isList}
+                      />
+                    </div>
                   </div>
-                </div>
-              ))
-            ) : (
-              <p>No plants match your search.</p>
-            )}
-            {errorFindingData && <Error />}
+                ))
+              : !errorFindingData && <p>No plants match your search.</p>}
+            {errorFindingData && createPortal(<ErrorPage />, document.body)}
             {isLoadingPlants && <Spinner />}
           </div>
         </div>
