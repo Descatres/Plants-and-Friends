0e133a04ee1b64d0d8744ff126c9fa44a499709f
João Catré <joaomcatre@hotmail.com>
2024-12-18 00:51:56 +0000

    Update RoomStats.tsx
    Co-Authored-By: Tiago Oliveira <69111007+toliveir4@users.noreply.github.com>

--- frontend/src/Components/RoomStats/RoomStats.tsx
+++ frontend/src/Components/RoomStats/RoomStats.tsx
@@ -20,6 +20,20 @@ function RoomStats() {
   //   return () => clearInterval(interval);
   // }, [getTemperature, getHumidity]);
 
+  const eventSource = new EventSource(
+    "http://localhost:5001/api/sensor-stream"
+  );
+
+  eventSource.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    console.log("Received Sensor Data:", data);
+  };
+
+  eventSource.onerror = (error) => {
+    console.error("Error with SSE:", error);
+    eventSource.close();
+  };
+
   return (
     <div className={classes.mainContainer}>
       <div className={classes.statsContainer}>
